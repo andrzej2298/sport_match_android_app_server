fff6587a3696cc88862e1651b7898ff7415eecef
Andrzej Michnowski <andrzej2298@gmail.com>
2020-01-10 21:29:43 +0100

    control docker container start order
    The django docker container exits when it can't connect to the database.
    The wait-for-it.sh script ensures that django starts only when
    it can connect to the database.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Apply database migrations
-echo "Apply database migrations"
-python manage.py migrate
-
-# Start server
-echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+./wait-for-it.sh db:5432 -- python manage.py migrate && python manage.py runserver 0.0.0.0:8000
