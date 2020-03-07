5ec4f1c1c6d3c871b5f51e1041305a24564a3ada
Andrzej Michnowski <andrzej2298@gmail.com>
2020-03-07 17:25:29 +0100

    add mock api endpoints with examples of usage
    Version 0.2 mock endpoints are added to the api,
    as well as a JSON file which documents usage.

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views.userviewset import UserViewSet
+from .views.userviewset import UserViewSet, MockUserViewset
 from .views.workoutviewset import WorkoutViewSet, MatchingWorkoutViewSet
 from .views.sportviewset import SportViewSet
 from .views.usersportviewset import UserSportViewSet
 
-from . import views
+from .views.loginviewset import MockLoginViewSet
+from .views.registerviewset import MockRegisterViewSet
+from .views.workoutviewset import MockWorkoutViewSet
+from .views.participationrequestviewset import MockParticipationRequestViewSet
 
+mock_router = routers.DefaultRouter()
 router = routers.DefaultRouter()
+
+mock_router.register(r'login', MockLoginViewSet, basename='login')
+mock_router.register(r'register', MockRegisterViewSet, basename='register')
+mock_router.register(r'users', MockUserViewset, basename='users')
+mock_router.register(r'workouts', MockWorkoutViewSet, basename='workouts')
+mock_router.register(
+    r'participation_requests', MockParticipationRequestViewSet,
+    basename='participation_requests'
+)
+
 router.register(r'users', UserViewSet)
 router.register(r'workouts', WorkoutViewSet)
 router.register(r'sports', SportViewSet)
@@ -20,6 +34,6 @@ router.register(
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('mock/', include(mock_router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
