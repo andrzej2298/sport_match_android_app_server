9122158ff32affede5b68eb83b9e97a6d556b536
Andrzej Michnowski <andrzej2298@gmail.com>
2020-06-01 16:42:21 +0200

    add suggestions tests

--- api/test_cases/base.py
+++ api/test_cases/base.py
@@ -4,20 +4,55 @@ Abstract test case class that implements useful methods used in real test cases.
 
 from rest_framework.test import APITestCase
 from rest_framework import status
+from sys import stderr
 
+from api.models.constants import ACCEPTED
 from .data import *
 
 
-class TestBase(APITestCase):
-    def create_user(self):
-        self.user = self.register_user(JOHN)
+def err(*args):
+    print(*args, file=stderr)
 
-    def authenticate_user(self):
-        token = self.get_token(JOHNS_CREDENTIALS)
+
+class TestBase(APITestCase):
+    def create_user(self, user):
+        self.user = self.register_user(user)
+
+    def authenticate_user(self, user_credentials):
+        token = self.get_token(user_credentials)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
 
     def reset_authentication(self):
         self.client.credentials()
+
+    def get_suggestions(self):
+        return self.client.get('/api/suggested_workouts/', format='json')
+
+    def get_sport_suggestions(self, sport_id):
+        return self.client.get(
+            '/api/suggested_workouts/',
+            {'sport': sport_id},
+            format='json',
+        )
+
+    def post_request(self, workout_id):
+        return self.client.post(
+            '/api/participation_requests/',
+            {
+                'workout': workout_id,
+                'message': 'abc',
+            },
+            format='json',
+        )
+
+    def accept_request(self, request_id):
+        return self.client.patch(
+            f'/api/participation_requests/{request_id}/',
+            {
+                'status': ACCEPTED,
+            },
+            format='json',
+        )
 
     def add_sport(self, sport, success=True) -> int:
         return self.add_row(sport, '/api/sports/', success)
@@ -33,19 +68,23 @@ class TestBase(APITestCase):
 
     def get_token(self, user_credentials, success=True) -> str:
         response = self.client.post('/api/login/', user_credentials, format='json')
-        self.check(response.status_code, status.HTTP_200_OK, success)
+        self.check(response, status.HTTP_200_OK, success)
         if success:
             return response.data['token']
 
-    def check(self, status_code, expected_code, success):
+    def check(self, response, expected_code, success):
         if success:
-            self.assertEqual(status_code, expected_code)
+            if response.status_code != expected_code and hasattr(response, 'data'):
+                err(response.data)
+            self.assertEqual(response.status_code, expected_code)
         else:
-            self.assertNotEqual(status_code, expected_code)
+            if response.status_code == expected_code and hasattr(response, 'data'):
+                err(response.data)
+            self.assertNotEqual(response.status_code, expected_code)
 
     def add_row(self, item, url, success) -> int:
         response = self.client.post(url, item, format='json')
-        self.check(response.status_code, status.HTTP_201_CREATED, success)
+        self.check(response, status.HTTP_201_CREATED, success)
 
         if success:
             return response.data['id']
--- api/test_cases/tokens.py
+++ api/test_cases/tokens.py
@@ -4,14 +4,14 @@ from .data import *
 
 class TokenTest(TestBase):
     def setUp(self) -> None:
-        self.create_user()
+        self.create_user(JOHN)
 
     def test_permissions(self) -> None:
         # first try without permissions
         self.add_user_sport(JOHNS_RUNNING, success=False)
 
         # try with permissions
-        self.authenticate_user()
+        self.authenticate_user(JOHNS_CREDENTIALS)
         self.add_user_sport(JOHNS_RUNNING, success=True)
         self.reset_authentication()
 
@@ -19,6 +19,6 @@ class TokenTest(TestBase):
         self.add_workout(MIM_WORKOUT, success=False)
 
         # again with permissions
-        self.authenticate_user()
+        self.authenticate_user(JOHNS_CREDENTIALS)
         self.add_workout(MIM_WORKOUT, success=True)
         self.reset_authentication()
