9dac8fb1532986c1187d6a019dece2a8a6335c21
Andrzej Michnowski <andrzej2298@gmail.com>
2020-06-01 17:02:47 +0200

    add custom username already taken message

--- api/utils/exception_handler.py
+++ api/utils/exception_handler.py
@@ -8,6 +8,15 @@ def _get_key(response, field):
         return response.data[field]
 
 
+def _prettify_result(error_message):
+    if (error_message['error_key'] == 'must_be_unique' and
+            error_message['error_fields'] and
+            error_message['error_fields'][0] == 'username'
+    ):
+        error_message['error_key'] = 'username_taken'
+        error_message['error_fields'] = []
+
+
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     error_key = ''
@@ -28,8 +37,12 @@ def custom_exception_handler(exc, context):
         error_key = split[0]
         error_fields = split[1].split(', ')
 
-    response.data = {
+    error_message = {
         'error_key': error_key,
         'error_fields': error_fields,
     }
+
+    _prettify_result(error_message)
+    response.data = error_message
+
     return response
